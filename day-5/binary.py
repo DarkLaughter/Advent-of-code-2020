72ea6232072012ee9db229745fad4c15dbe48bf9
Ramon Echeverria <ramon.l.echeverria@gmail.com>
2020-12-08 22:46:29 -0500

    part 1

--- day-5/binary.py
+++ day-5/binary.py
@@ -3,33 +3,37 @@ import math
 def find_midpoint(num):
     return math.ceil(num/2)
 
-def row_finder(char, num):
-    if char == 'F':
-        row_nums = find_midpoint(num)
-    elif char == 'B':
-        num = find_midpoint(num)
-
-def seat_finder(char, num):
-    if char == 'L':
-        row_nums = find_midpoint(num)
-    elif char == 'B' or char == 'R':
-        num = find_midpoint(num)
-
-def highest_seat_num(list):
+def row_seat_decoder(list):
+    seat_dict = {}
     for item in list:
-        row_info = item[0:-3]
-        row_nums = 127
-        seat_info = item[-3:]
-        seat_nums = 7
-        for char in row_info:
-            row_finder(char,row_nums)
-        for char in seat_info:
-            row_finder(char, seat_nums)
-        print(row_nums, seat_nums)
+        rows = [*range(1,128)]
+        seats = [*range(1,8)]
+        for char in item[0:-3]:
+            if char == 'F':
+                rows = rows[:find_midpoint(len(rows))]
+            elif char == 'B':
+                rows = rows[find_midpoint(len(rows)):]
+        for char in item[-3:]:
+            if char == 'L':
+                seats = seats[:find_midpoint(len(seats))]
+            elif char == 'R':
+                if len(seats) == 1:
+                    break
+                seats = seats[find_midpoint(len(seats)):]
+        seat_dict[rows[0]] = seats[0]
+    return seat_dict 
 
+def highest_id(lst):
+    current_tickets = row_seat_decoder(lst)
+    highest_id = 0
+    for i in current_tickets:
+        id = i * 8 + current_tickets[i]
+        if highest_id < id:
+            highest_id = id
+    return highest_id
 
 with open('input.txt') as input:
     seat_assignments = input.read().rstrip()
     seat_lst = seat_assignments.split("\n")
 
-print(highest_seat_num(seat_lst))
+print(highest_id(seat_lst))
